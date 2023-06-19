af18bea60d1ff4bc6524243636b4fbdd03e28a11
novita <ekasarinovita4@gmail.com>
2023-06-19 20:20:04 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Novita Ekasari"; // TODO: replace this
+const studentId = "FE5684125"; // TODO: replace this
+
+export { url, studentName, studentId };
